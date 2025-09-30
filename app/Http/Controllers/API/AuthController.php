ef409ab81c02b93bb90a991467a488a78d1a8b7e
rasel-ahmmed <raselahmmed247@gmail.com>
2025-10-01 00:34:01 +0600

    feat: Add role-based authorization

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -10,50 +10,50 @@ use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
+      public function register(Request $request)
+      {
+          $validatedData = $request->validate([
+              'name' => 'required|string|max:255',
+              'email' => 'required|string|email|max:255|unique:users',
+              'password' => 'required|string|min:8',
+              'role' => 'sometimes|in:admin,customer'
+          ]);
+  
+          $user = User::create([
+              'name' => $validatedData['name'],
+              'email' => $validatedData['email'],
+              'password' => Hash::make($validatedData['password']),
+              'role' => $validatedData['role'] ?? 'customer',
+          ]);
+    $token = $user->createToken('auth_token')->plainTextToken;
 
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-        ]);
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'Bearer',
+    ]);
+  }
 
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
+  public function login(Request $request)
+  {
+    if (!auth()->attempt($request->only('email', 'password'))) {
+      return response()->json([
+        'message' => 'Invalid login details'
+      ], 401);
     }
 
-    public function login(Request $request)
-    {
-        if (!auth()->attempt($request->only('email', 'password'))) {
-            return response()->json([
-                'message' => 'Invalid login details'
-            ], 401);
-        }
+    $user = User::where('email', $request['email'])->firstOrFail();
 
-        $user = User::where('email', $request['email'])->firstOrFail();
+    $token = $user->createToken('auth_token')->plainTextToken;
 
-        $token = $user->createToken('auth_token')->plainTextToken;
+    return response()->json([
+      'access_token' => $token,
+      'token_type' => 'Bearer',
+    ]);
+  }
 
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
-    }
-
-    public function logout(Request $request)
-    {
-        $request->user()->currentAccessToken()->delete();
-
-        return response()->json(['message' => 'Logged out']);
-    }
+  public function logout(Request $request)
+  {
+    $request->user()->currentAccessToken()->delete();
+    return response()->json(['message' => 'Logged out']);
+  }
 }
