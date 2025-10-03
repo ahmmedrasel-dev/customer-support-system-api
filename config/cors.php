9bd5983561da6a23474d81c2e763f5ded850e86b
rasel-ahmmed <raselahmmed247@gmail.com>
2025-10-03 17:23:11 +0600

    remove docker and render config

--- config/cors.php
+++ config/cors.php
@@ -19,11 +19,7 @@ return [
 
   'allowed_methods' => ['*'],
 
-  'allowed_origins' => [
-    'http://localhost:3000',
-    'https://localhost:3000',
-    env('FRONTEND_URL', 'http://localhost:3000'),
-  ],
+  'allowed_origins' => ['http://localhost:3000'],
 
   'allowed_origins_patterns' => [],
 
